45bd79f7ba732a4a33bff3b9224ccdfd4840b83d
David Hoelscher <infinityis@users.noreply.github.com>
2023-10-24 23:56:58 -0500

    fix default layer persistence to support up to 32 single layer or 8 bitmasked layers

--- keyboards/helix/rev2/keymaps/froggy/keymap.c
+++ keyboards/helix/rev2/keymaps/froggy/keymap.c
@@ -187,11 +187,6 @@ float tone_plover_gb[][2]  = SONG(PLOVER_GOODBYE_SOUND);
 //bool TOG_STATUS = false;
 int RGB_current_mode;
 
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
 bool find_mairix(uint16_t keycode, uint8_t *row, uint8_t *col){
   for(uint8_t i=0; i<MATRIX_ROWS; i++){
     for(uint8_t j=0; j<MATRIX_COLS; j++){
